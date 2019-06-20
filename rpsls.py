2b7d8a1d6192fd4d0287cd6b897ef4940218397f
jmcasebier <jason.beercase@gmail.com>
2019-06-19 21:37:39 -0500

    added rules screen

--- rpsls.py
+++ rpsls.py
@@ -12,7 +12,7 @@ def clear():
         _ = system('clr')
 
 #begin the game
-def beginGame():
+def begin_game():
     clear()
     #introduce game
     print('Welcome to Rock-Paper-Scissors-Lizard-Spock!')
@@ -20,45 +20,63 @@ def beginGame():
     get_moves()
 
 #present option to play again
-def gameOver():
-    playAgain = input('\nPlay again? (y or n): ')
+def game_over():
+    play_again = input('\nPlay again? (y or n): ')
     #verify yes or no response
-    yesOrNo = ('y', 'n', 'yes', 'no')
-    while playAgain.lower() not in yesOrNo:
-        playAgain = input('\nPlease enter y to play again or n to exit: ')
+    yes_or_no = ('y', 'n', 'yes', 'no')
+    while play_again.lower() not in yes_or_no:
+        play_again = input('\nPlease enter y to play again or n to exit: ')
 
     #respond to user selection
-    if playAgain.lower() == 'y' or playAgain.lower() == 'yes':
-        beginGame()
+    if play_again.lower() == 'y' or play_again.lower() == 'yes':
+        begin_game()
     else:
         clear()
         exit()
 
+#display rules on request
+def show_rules():
+    clear()
+    rules = '***Rules***\n\nScissors cut Paper,\nPaper covers Rock,\nRock '
+    rules += 'crushes Lizard,\nLizard poisons Spock,\nSpock smashes Scissors,'
+    rules += '\nScissors decapitate Lizard,\nLizard eats Paper,\nPaper '
+    rules += 'disproves Spock,\nSpock vaporizes Rock,\nand as it always has,\n'
+    rules += 'Rock crushes Scissors.\n\n'
+    print(rules)
+    #prompt user to close rules with q
+    quit_rules = input('Press Enter to return to the game...')
+    begin_game()
+
 #introduce all possible moves
 def show_moves():
-    print('\n1 - Rock\n2 - Paper\n3 - Scissors\n4 - Lizard\n5 - Spock\n')
+    print('\n1 - Rock\n2 - Paper\n3 - Scissors\n4 - Lizard\n5 - Spock\n'
+    + '# - Rules\n')
 
 #prompt user for move selection
 def get_moves():
     user_move = input('Make your selection: ')
     #verify user selection validity
-    moves = ('1', '2', '3', '4', '5')
+    moves = ('1', '2', '3', '4', '5', '#')
     while user_move not in moves:
         user_move = input('\nPlease enter the number of your selection: ')
 
-    #generate computer selection
-    comp_move = str(randint(1, 5))
-    if comp_move == '1':
-        comp = 'Rock'
-    elif comp_move == '2':
-        comp = 'Paper'
-    elif comp_move == '3':
-        comp = 'Scissors'
-    elif comp_move == '4':
-        comp = 'Lizard'
-    elif comp_move == '5':
-        comp = 'Spock'
-    compare_moves(user_move, comp_move, comp)
+    #handle user option to show rules
+    if user_move == '#':
+        show_rules()
+    else:
+        #generate computer selection
+        comp_move = str(randint(1, 5))
+        if comp_move == '1':
+            comp = 'Rock'
+        elif comp_move == '2':
+            comp = 'Paper'
+        elif comp_move == '3':
+            comp = 'Scissors'
+        elif comp_move == '4':
+            comp = 'Lizard'
+        elif comp_move == '5':
+            comp = 'Spock'
+        compare_moves(user_move, comp_move, comp)
 
 #compare moves
 def compare_moves(user_move, comp_move, comp):
@@ -116,7 +134,7 @@ def display_winner(comp_win, user, comp):
         print('\nYou Win.')
     else:
         print('\nIt\'s a tie.')
-    gameOver()
+    game_over()
 
 #initial run
-beginGame()
+begin_game()
