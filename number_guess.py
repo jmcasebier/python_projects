730e90a01c85dd8e2f9fa2c4bb2250b22850d167
jmcasebier <jason.beercase@gmail.com>
2019-06-27 20:15:22 -0500

    number guess initial commit

---
+++ number_guess.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+#imports
+from random import randint
+
+guessesTaken = 0
+
+print('Hello! What is your name?')
+name = input()
+
+number = randint(1, 20)
+print('Well, ' + name + ', I am thinking of a number between 1 and 20.')
+
+while guessesTaken < 6:
+    if guessesTaken == 0:
+        print('Take a guess.')
+    else:
+        print('Take another guess.')
+
+    guess = int(input())
+    guessesTaken += 1
+
+    if guess < number:
+        print('Your guess is too low.')
+    if guess > number:
+        print('Your guess is too high.')
+    if guess == number:
+        break
+
+if guess == number:
+    guessesTaken = str(guessesTaken)
+    print('Good job, ' + name + '! You guessed the number in ' +
+    guessesTaken + ' guesses!')
+
+if guess != number:
+    number = str(number)
+    print('Nope. The number I was thinking of was ' + number + '.')
