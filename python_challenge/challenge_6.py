25ad854b527b5fe180b9da62786e0bba376e5bf7
Jason Casebier <35588924+jmcasebier@users.noreply.github.com>
2020-02-05 11:27:43 -0600

    Update challenge_6.py

--- python_challenge/challenge_6.py
+++ python_challenge/challenge_6.py
@@ -1,1 +1,22 @@
 #!/usr/bin/env python3
+
+import zipfile
+import re
+
+file = zipfile.ZipFile("channel.zip")
+print(f.read("readme.txt").decode("utf-8"))
+
+num = '90052'
+
+comments = []
+
+while True:
+    content = file.read(num + ".txt").decode("utf-8")
+    comments.append(file.getinfo(num + ".txt").comment.decode("utf-8"))
+    print(content)
+    match = re.search("Next nothing is (\d+)", content)
+    if match == None:
+        break
+    num = match.group(1)
+
+print("".join(comments))
